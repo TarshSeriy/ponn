d38acdeacee1e20abdc21f81aa38cd0b6be5b733
TarshSeriy <154108692+TarshSeriy@users.noreply.github.com>
2025-06-24 20:18:45 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+BOT_TOKEN = "8199978106:AAFa0901MxABGaQLjrWPdXiliOkqsgKhj2M"
+
+async def start(update: Update, context):
+    keyboard = [
+        [KeyboardButton("🎉 Открыть поздравление", web_app=WebAppInfo(url="https://pon-alpha.vercel.app/"))]
+    ]
+    await update.message.reply_text(
+        "С днюшкой твоей подруг! Жми кнопку 🎈",
+        reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
