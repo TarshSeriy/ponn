4cdb0c6a6451f5bd3e09be3f795a3255a1022978
TarshSeriy <154108692+TarshSeriy@users.noreply.github.com>
2025-06-24 23:40:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
-from telegram.ext import ApplicationBuilder, CommandHandler
-from dotenv import load_dotenv
 import os
-
+import requests
+from dotenv import load_dotenv
+from telegram import Update, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
-async def start(update: Update, context):
+# Поздравление (с веб-приложением)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [KeyboardButton("🎉 Йоооооооо нажимай жоск", web_app=WebAppInfo(url="https://pon-alpha.vercel.app/"))]
+        [KeyboardButton(text="🎉 Открыть сюрприз", web_app=WebAppInfo(url="https://pon-gprfbhkrr-sergeys-projects-f4e91b78.vercel.app/"))],
     ]
-    await update.message.reply_text(
-        "С днем рождения красотка! Жоск жми 🎈",
-        reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-    )
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    await update.message.reply_text("Привет! Я Кубарсыч 🎁\nНажми кнопку, чтобы открыть сюрприз!", reply_markup=reply_markup)
 
+# Котик
+async def cat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        response = requests.get("https://api.thecatapi.com/v1/images/search")
+        data = response.json()
+        cat_url = data[0]["url"]
+        await update.message.reply_photo(cat_url, caption="Вот тебе котик 😻")
+    except Exception as e:
+        await update.message.reply_text("Что-то пошло не так, котик убежал 😿")
+
+# Бот
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("котик", cat))
+
 app.run_polling()
